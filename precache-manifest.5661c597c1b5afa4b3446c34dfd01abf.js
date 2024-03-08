0e9f885d1d8670b8d1438dafffb19ff9231d7a52
akshay.agrawal <akshay.agrawal2504@gmail.com>
2024-03-08 17:38:18 +0530

    Updates

---
+++ precache-manifest.5661c597c1b5afa4b3446c34dfd01abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58d7731db93e7ac949038d82cf5a17d",
+    "url": "/news-article-app/index.html"
+  },
+  {
+    "revision": "222137536fa5d72120d6",
+    "url": "/news-article-app/static/css/main.5369c7d6.chunk.css"
+  },
+  {
+    "revision": "3533bb0c1f39ee1ae81d",
+    "url": "/news-article-app/static/js/2.3c3a0fc0.chunk.js"
+  },
+  {
+    "revision": "0974ae7ba865449b1326",
+    "url": "/news-article-app/static/js/3.946c96ab.chunk.js"
+  },
+  {
+    "revision": "222137536fa5d72120d6",
+    "url": "/news-article-app/static/js/main.fe2ee2b4.chunk.js"
+  },
+  {
+    "revision": "f9f72e13128af24d48ce",
+    "url": "/news-article-app/static/js/runtime~main.b825ce43.js"
+  }
+]);
